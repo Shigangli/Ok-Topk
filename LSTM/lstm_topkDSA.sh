124a7b0d4123280f28441a29fdc658ac07307f28
Shigang Li <shigang.li@inf.ethz.ch>
2021-12-11 23:21:42 +0100

    lstm script

--- LSTM/lstm_topkDSA.sh
+++ LSTM/lstm_topkDSA.sh
@@ -11,19 +11,7 @@
 
 
 module load daint-gpu
-module load cudatoolkit/10.2.89_3.29-7.0.2.1_3.27__g67354b4
-
-__conda_setup="$('/project/g34/shigang/anaconda38/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/project/g34/shigang/anaconda38/etc/profile.d/conda.sh" ]; then
-        . "/project/g34/shigang/anaconda38/etc/profile.d/conda.sh"
-    else
-        export PATH="/project/g34/shigang/anaconda38/bin:$PATH"
-    fi
-fi
-unset __conda_setup
+conda activate py38_oktopk
 which nvcc
 nvidia-smi
 
